8f3b78fea45d08678293f441568cac0f30d8d5bf
blair-js <81502408+blair-js@users.noreply.github.com>
2021-06-19 22:45:35 +0900

    boolean, null, undefined

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-// const mon = "mon";
-// const tue = "tue";
-// const wed = "wed";
-// const thu = "thu";
-// const fri = "fri";
-// const sat = "sat";
-// const sun = "sun";
+const a = 5;
+const b = 2;
 
-//const dayOfWeek = mon + tue + wed + thu + fri + sat + sun;
-//montuewedthufrisatsun 으로 출력됨. 잘못된 array. 
+let myName = "blair"; 
 
-// const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
 
-// const nonsense = [1, 2, "hello", false, null, true, undefined, "blair"];
-
-// console.log(daysOfWeek, nonsense);
-
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "Sat"];
-//console.log(daysOfWeek);
-
-//Get Item from Array 
-//console.log(daysOfWeek[5]); //5번째 찾기 
-console.log(daysOfWeek);
-
-//Add one more day to the array
-daysOfWeek.push("sun");
-console.log(daysOfWeek);
- 
-
-//xxx.xx 
-
+myName= "jisu"
+console.log("your new name is " +myName);
