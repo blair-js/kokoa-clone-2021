67fa22b534c3f75b86e0f4895550fce68f2127eb
blair-js <81502408+blair-js@users.noreply.github.com>
2021-06-20 21:25:58 +0900

    Object

---
+++ app.js
@@ -0,0 +1,21 @@
+//  const playerName = "blair";
+//  const playerPoints = 121212;
+//  const playerHandsome = false;
+//  const playerFat = "little bit";
+ 
+
+const player = { 
+    name: "blair",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+player.fat = false;
+player.lastname = "potato";
+player.points = player.points + 15;
+console.log(player.points);
+console.log(player);
+// console.log(player.name);
+// console.log(player["name"]);
+
+
