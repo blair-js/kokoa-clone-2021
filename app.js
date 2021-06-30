881c19d0ac0356e8c29d2db3aa9fb847702c3600
blair-js <81502408+blair-js@users.noreply.github.com>
2021-06-30 23:50:21 +0900

    3

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const clickClass = "clicked";
-   if(h1.className === "clickClass") {
-       h1.className = "";
-   } else {
-       h1.className = "clickClass";
-   }
+   
+   h1.classList.toggle("clicked");
 }
-
-
 
 h1.addEventListener("click", handleTitleClick);
 // title.onclick = handleTitleClick; ===> 위와 같은 기능 
