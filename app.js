ba69698f052d6b81b072ec00959ab631fc106142
blair-js <81502408+blair-js@users.noreply.github.com>
2021-07-01 23:20:57 +0900

    0701

--- app.js
+++ app.js
@@ -5,8 +5,19 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
+// function onLoginBtnClick() {
+//      const username = loginInput.value;
+//      if(username === "") {
+//         alert("Please write your name.");
+//      } else if(username.length > 15) {
+//         alert("Your name is too long.");
+//      }
+// }
+//위의 방법도 좋지만 최대한 브라우저의 기능을 활용하는 것이 좋음. 하기의 코드 참조. 
+
 function onLoginBtnClick() {
-     console.log(loginInput.value);
+   const username = loginInput.value;
+   console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick)
