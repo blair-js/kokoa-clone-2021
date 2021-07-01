063b9be739c1958d0cf774022d56a505c8e87c93
blair-js <81502408+blair-js@users.noreply.github.com>
2021-07-01 22:59:41 +0900

    Log-in

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
 
-function handleTitleClick() {
-   
-   h1.classList.toggle("clicked");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+     console.log(loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
-// title.onclick = handleTitleClick; ===> 위와 같은 기능 
+loginButton.addEventListener("click", onLoginBtnClick)
