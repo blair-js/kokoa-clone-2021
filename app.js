ef6353b772d26a91b3c6f76133f84a0bc7292c5e
blair-js <81502408+blair-js@users.noreply.github.com>
2021-07-02 20:45:17 +0900

    preventDefault

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 // const loginInput = loginForm.querySelector("input");
 // const loginButton = loginForm.querySelector("button");
 
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+// const loginButton = document.querySelector("#login-form button");
 
 // function onLoginBtnClick() {
 //      const username = loginInput.value;
@@ -15,9 +16,10 @@ const loginButton = document.querySelector("#login-form button");
 // }
 //위의 방법도 좋지만 최대한 브라우저의 기능을 활용하는 것이 좋음. 하기의 코드 참조. 
 
-function onLoginBtnClick() {
-   const username = loginInput.value;
-   console.log(username);
+function onLoginSubmit(event) {
+   event.preventDefault();
+   console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick)
+loginForm.addEventListener("submit", onLoginSubmit);
+
