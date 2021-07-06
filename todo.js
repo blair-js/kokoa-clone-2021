221f21dccd3c2ca0c4c4cee645cf8bde63bab0e7
blair-js <81502408+blair-js@users.noreply.github.com>
2021-07-07 08:58:50 +0900

    0707

--- todo.js
+++ todo.js
@@ -3,11 +3,19 @@ const toDoInput = toDoForm.querySelector("input");
 //const toDoInput = document.querySelector("todo-form input"); 위 코드와 동일한내용. 코드법만 다름. 
 const toDoList = document.getElementById("todo-list");
 
+function painToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDosubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    console.log(newTodo, toDoInput.value);
+    painToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDosubmit);
