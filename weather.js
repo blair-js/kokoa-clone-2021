fc72e58ff32c66989c12b5b64cd4365f3308c255
blair-js <81502408+blair-js@users.noreply.github.com>
2021-07-13 20:51:43 +0900

    weather

--- weather.js
+++ weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "c3b23636376dacd84cd5e11612108a86";
+
+
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-   console.log("You live it", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+       const weather = document.querySelector("#weather span:first-child");
+       const city = document.querySelector("#weather span:last-child");
+       city.innerText = data.name;
+       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
    alert("Can't find you. No weather for you.");
